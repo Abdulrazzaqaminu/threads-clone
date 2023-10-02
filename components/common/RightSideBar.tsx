2a701613279c27720e965d2aacb7238cad840edf
Abdulrazzaqaminu <abdulrazzaqaminu@gmail.com>
2023-10-02 19:32:33 +0100

    home page

--- components/common/RightSideBar.tsx
+++ components/common/RightSideBar.tsx
@@ -1,5 +1,12 @@
 export default function RightSideBar() {
   return (
-    <div>RightSideBar</div>
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">Suggested communities</h3>
+      </div>
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">Suggested users</h3>
+      </div>
+    </section>
   )
 }
